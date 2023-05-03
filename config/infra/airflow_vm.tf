220740021e83a99b591d9ca91bf26662f6e3eea7
Jose Adelmar <j.aldemar@4intelligence.com.br>
2023-05-03 11:25:30 -0300

    test env var

--- config/infra/airflow_vm.tf
+++ config/infra/airflow_vm.tf
@@ -13,9 +13,16 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
   metadata_startup_script = <<-SCRIPT
+  mkdir /git && mkdir /git/gce && \
+  export GOOGLE_APPLICATION_CREDENTIALS=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/TF_VAR_GOOGLE_APPLICATION_CREDENTIALS" -H "Metadata-Flavor: Google")
+  echo $GOOGLE_APPLICATION_CREDENTIALS > /git/test_env_var.txt && \
   gsutil rsync -r gs://dataflow_jose /git/gce && \
   echo "sudo gsutil rsync -r gs://dataflow_jose /git/gce" >> /git/sync.sh && \
   echo "*/5 * * * * sudo gsutil rsync -r gs://dataflow_jose /git/gce" | crontab -
   SCRIPT
+
+  metadata = {
+    "TF_VAR_project_id" = var.GOOGLE_APPLICATION_CREDENTIALS
+  }
 
 }
